dd81bd405c759042f61221bacb276ceb87ff9733
Shankar Gairy <skgairy1110@gmail.com>
2026-05-24 04:57:22 +0530

    updated project

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    tsConfigPaths(),
+    tailwindcss(),
+    tanstackStart({
+      target: "vercel",
+    }),
+    react(),
+  ],
+});
