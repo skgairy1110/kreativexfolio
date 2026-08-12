3cc34f92f5d000b343d1344750cb478ab85b138d
Shankar Gairy <skgairy1110@gmail.com>
2026-08-12 22:18:13 +0530

    work index update

--- src/router.tsx
+++ src/router.tsx
@@ -8,7 +8,8 @@ export const getRouter = () => {
   const router = createRouter({
     routeTree,
     context: { queryClient },
-    scrollRestoration: true,
+    scrollRestoration: false,
+    scrollToTopSelectors: ["html", "body"],
     defaultPreloadStaleTime: 0,
   });
 
